1e3a752ee157b6d8dcb7bc125e93ecb080a2987f
Prem N Chalase <premchalase3@gmail.com>
2026-03-12 17:54:16 +0530

    fixed groq api 2nd time

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -5,24 +5,24 @@ export const financeChatbot = async (req, res) => {
   try {
 
     const groq = new Groq({
-      apiKey: process.env.GROQ_API_KEY,
+      apiKey: process.env.GROQ_API_KEY
     });
 
     const { message } = req.body;
 
     const completion = await groq.chat.completions.create({
+      model: "llama-3.1-8b-instant",
       messages: [
         {
           role: "system",
           content:
-            "You are a financial advisor helping users manage their expenses and savings."
+            "You are a financial advisor helping users manage their expenses, savings, and budgeting."
         },
         {
           role: "user",
           content: message
         }
-      ],
-      model: "llama3-8b-8192"
+      ]
     });
 
     res.json({
