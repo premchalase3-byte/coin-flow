0c2b17200ac84b3f258c79a6d1576fa3c1659ed1
Prem N Chalase <premchalase3@gmail.com>
2026-03-30 00:29:13 +0530

    updated new AI model

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -82,7 +82,7 @@ If overspending exists, suggest improvement tips.
     /* ========================= */
 
     const completion = await groq.chat.completions.create({
-      model: "mixtral-8x7b-32768",
+      model: "llama-3.3-70b-versatile",
       messages: [
         {
           role: "user",
